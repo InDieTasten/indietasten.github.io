2160492c43be6a7f4457683989ac9e8ca96aff13
InDieTasten <indietasten@gmail.com>
2025-01-12 15:25:45 +0100

    Add favicon
    Closes #146

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -10,60 +10,39 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
-  openGraph: {
-    images: [HOME_OG_IMAGE_URL],
-  },
+    title: `Next.js Blog Example with ${CMS_NAME}`,
+    description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+    openGraph: {
+        images: [HOME_OG_IMAGE_URL],
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#000000"
-        />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#000000" />
-        <meta
-          name="msapplication-config"
-          content="/favicon/browserconfig.xml"
-        />
-        <meta name="theme-color" content="#000" />
-        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      </head>
-      <body
-        className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}
-      >
-        <ThemeSwitcher />
-        <div className="min-h-screen">{children}</div>
-        <Footer />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png?v=2" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png?v=2" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png?v=2" />
+                <link rel="manifest" href="/icons/site.webmanifest?v=2" />
+                <link rel="mask-icon" href="/icons/safari-pinned-tab.svg?v=2" color="#00ff00" />
+                <link rel="shortcut icon" href="/icons/favicon.ico?v=2" />
+                <meta name="msapplication-TileColor" content="#535353" />
+                <meta name="msapplication-config" content="/icons/browserconfig.xml?v=2" />
+                <meta name="theme-color" content="#ffffff" />
+                <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+            </head>
+            <body
+                className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}
+            >
+                <ThemeSwitcher />
+                <div className="min-h-screen">{children}</div>
+                <Footer />
+            </body>
+        </html>
+    );
 }
