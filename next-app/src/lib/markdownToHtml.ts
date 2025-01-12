9d18dcb5f167bc5579de987d04380367be71ac02
InDieTasten <indietasten@gmail.com>
2025-01-12 16:54:27 +0100

    Add syntax-highlighting
    Closes #145

--- next-app/src/lib/markdownToHtml.ts
+++ next-app/src/lib/markdownToHtml.ts
@@ -1,7 +1,16 @@
-import { remark } from "remark";
-import html from "remark-html";
+import { all } from '@wooorm/starry-night'
+import rehypeStarryNight from 'rehype-starry-night'
+import rehypeStringify from 'rehype-stringify'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import { unified } from 'unified'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
-  return result.toString();
+    const result = await unified()
+        .use(remarkParse)
+        .use(remarkRehype)
+        .use(rehypeStarryNight, { grammars: all })
+        .use(rehypeStringify)
+        .process(markdown);
+    return result.toString();
 }
