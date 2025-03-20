9d177642008867b4d8bd319e57dd5089279ae5c4
InDieTasten <indietasten@gmail.com>
2025-03-20 23:55:21 +0100

    feat: improve index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,6 @@ export default function Index() {
     return (
         <main>
             <Container>
-                <Intro />
                 <HeroPost
                     title={heroPost.title}
                     //   coverImage={heroPost.coverImage}
