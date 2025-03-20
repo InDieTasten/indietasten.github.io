c7e53e8bd78e4a69f58a2a5cd4be93936af46a29
InDieTasten <indietasten@gmail.com>
2025-03-20 23:37:15 +0100

    feat: basic layouting
    Refs: #142
    Refs: #143
    Refs: #144

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -2,7 +2,7 @@ import Container from "@/app/_components/container";
 
 export function Footer() {
   return (
-    <footer className="bg-neutral-50 border-t border-neutral-200 dark:bg-slate-800">
+    <footer className="border-t border-neutral-200 bg-slate-800">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,43 +6,42 @@ import cn from "classnames";
 import { ThemeSwitcher } from "./_components/theme-switcher";
 
 import "./globals.css";
+import { Layout } from "@/components/Layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: SITE_TITLE,
-    description: `Homepage and blog by InDieTasten.`,
-    openGraph: {
-        images: [HOME_OG_IMAGE_URL],
-    },
+  title: SITE_TITLE,
+  description: `Homepage and blog by InDieTasten.`,
+  openGraph: {
+    images: [HOME_OG_IMAGE_URL],
+  },
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <head>
-                <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png?v=2" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png?v=2" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png?v=2" />
-                <link rel="manifest" href="/icons/site.webmanifest?v=2" />
-                <link rel="mask-icon" href="/icons/safari-pinned-tab.svg?v=2" color="#00ff00" />
-                <link rel="shortcut icon" href="/icons/favicon.ico?v=2" />
-                <meta name="msapplication-TileColor" content="#535353" />
-                <meta name="msapplication-config" content="/icons/browserconfig.xml?v=2" />
-                <meta name="theme-color" content="#ffffff" />
-                <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-            </head>
-            <body
-                className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}
-            >
-                <ThemeSwitcher />
-                <div className="min-h-screen">{children}</div>
-                <Footer />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png?v=2" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png?v=2" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png?v=2" />
+        <link rel="manifest" href="/icons/site.webmanifest?v=2" />
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg?v=2" color="#00ff00" />
+        <link rel="shortcut icon" href="/icons/favicon.ico?v=2" />
+        <meta name="msapplication-TileColor" content="#535353" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml?v=2" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      </head>
+      <body className="flex h-full bg-black">
+        <div className="flex w-full">
+          <Layout>{children}</Layout>
+        </div>
+      </body>
+    </html>
+  );
 }
