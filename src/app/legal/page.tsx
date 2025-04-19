aa7a3ca6f05388c62cae48be3f9a4d76b1ff4f16
InDieTasten <indietasten@gmail.com>
2025-04-19 04:53:40 +0000

    feat: legal page
    Refs: #157

---
+++ src/app/legal/page.tsx
@@ -0,0 +1,17 @@
+import { Container } from '@/components/Container';
+import { NextPage } from 'next'
+
+interface Props { }
+
+const LegalPage: NextPage<Props> = ({ }) => {
+    return (
+        <Container className="mt-9">
+            <h1 className="text-3xl font-bold tracking-tight sm:text-3xl text-zinc-400">The boring stuff</h1>
+            <h2 className="text-2xl font-bold tracking-tight sm:text-2xl text-zinc-100 my-5">3rd party links</h2>
+            <p>
+                This website contains links to third party websites and services for the convienience of the user. InDieTasten.net does not control these websites and has no association to the providers of these websites. If any such services or websites linked cause any harm or damages, the authors of InDieTasten.net are not liable. If users of InDieTasten follow these external links, they do so at their own risk.
+            </p>
+        </Container>);
+}
+
+export default LegalPage
